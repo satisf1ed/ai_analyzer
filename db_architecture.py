07fae6a51b22f48c93c00a550c901ad56b1ad51a
Satisf1ed <zhogov.andrey.d@gmail.com>
2024-10-27 13:37:09 +0300

    add docstrings to db_architecture.py

--- db_architecture.py
+++ db_architecture.py
@@ -8,6 +8,22 @@ Base = declarative_base()
 
 
 class Channel(Base):
+    """
+        Represents a YouTube channel with metadata such as title, description, and language.
+
+        Attributes:
+            id (BigInteger): Primary key identifier for the channel.
+            title (str): Title of the channel.
+            description (str): Description of the channel content.
+            publishedAt (Time): Time when the channel was published.
+            defaultLan (str): Default language for the channel.
+
+        Relationships:
+            videos (Video): Relationship with the `Video` class.
+            authored_comments (Comment): Relationship with comments authored by this channel.
+            received_comments (Comment): Relationship with comments received by this channel.
+        """
+
     __tablename__ = 'channels'
 
     id = Column(BigInteger, primary_key=True)
@@ -27,6 +43,27 @@ class Channel(Base):
 
 
 class Video(Base):
+    """
+        Represents a video uploaded to a YouTube channel.
+
+        Attributes:
+            id (BigInteger): Primary key identifier for the video.
+            publishedAt (Time): Timestamp for when the video was published.
+            channelId (BigInteger): Foreign key reference to the channel's ID.
+            title (str): Title of the video.
+            description (str): Description of the video content.
+            channelTitle (str): Name of the channel that published the video.
+            tags (JSON): JSON field containing tags associated with the video.
+            categoryId (BigInteger): ID of the video category.
+            defaultLanguage (str): Default language for the video.
+            duration (str): Duration of the video.
+
+        Relationships:
+            channel (Channel): Relationship with the `Channel` class.
+            subtitles (Subtitle): Relationship with subtitles associated with the video.
+            comments (Comment): Relationship with comments associated with the video.
+        """
+
     __tablename__ = 'videos'
 
     id = Column(BigInteger, primary_key=True)
@@ -53,6 +90,18 @@ class Video(Base):
 
 
 class Subtitle(Base):
+    """
+        Represents subtitles associated with a specific video.
+
+        Attributes:
+            id (BigInteger): Primary key identifier for the subtitle.
+            videoId (BigInteger): Foreign key reference to the associated video's ID.
+            text (str): The subtitle text.
+
+        Relationships:
+            video (Video): Relationship with the `Video` class.
+        """
+
     __tablename__ = 'subtitles'
 
     id = Column(BigInteger, primary_key=True)
@@ -67,6 +116,33 @@ class Subtitle(Base):
 
 
 class Comment(Base):
+    """
+       Represents a comment on a video, including author information and comment metadata.
+
+       Attributes:
+           id (BigInteger): Primary key identifier for the comment.
+           videoId (BigInteger): Foreign key reference to the video's ID the comment is associated with.
+           authorDisplayName (str): Display name of the comment's author.
+           authorChannelUrl (str): URL of the author's channel.
+           authorChannelId (BigInteger): Foreign key reference to the author's channel ID.
+           channelId (BigInteger): Foreign key reference to the channel receiving the comment.
+           textDisplay (str): Display text of the comment.
+           textOriginal (str): Original text of the comment.
+           parentId (BigInteger): Foreign key reference to the parent comment ID, if it is a reply.
+           canRate (bool): Indicates if the comment can be rated.
+           viewerRating (str): The viewer's rating on the comment.
+           likeCount (BigInteger): Number of likes on the comment.
+           moderationStatus (str): Moderation status of the comment.
+           publishedAt (Time): Timestamp for when the comment was published.
+           updatedAt (Time): Timestamp for when the comment was last updated.
+
+       Relationships:
+           video (Video): Relationship with the `Video` class.
+           author_channel (Channel): Relationship with the channel authoring the comment.
+           target_channel (Channel): Relationship with the channel receiving the comment.
+           parent_comment (Comment): Self-referential relationship for nested comments.
+       """
+
     __tablename__ = 'comments'
 
     id = Column(BigInteger, primary_key=True)
